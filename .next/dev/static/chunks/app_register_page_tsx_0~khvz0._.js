7c476dbccb47d12c09cf44388072f2200c6deef5
Atul <atul.raj@kreatetechnologies.com>
2026-03-23 13:42:28 +0530

    AMCSUPPORT1-1840:AMCSUPPORT1-184CRemove .next build c ache from repo

--- .next/dev/static/chunks/app_register_page_tsx_0~khvz0._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0osj3al._.js"],
-    source: "dynamic"
-});
